69f013f05b0a64bcd33de38bca6877ea5296a070
Dvd-coder-art <daviddavi1221@gmail.com>
2025-02-09 11:07:25 -0300

    Initial commit

---
+++ src/examples/ConsumidorExample.java
@@ -0,0 +1,24 @@
+package examples;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class ConsumidorExample {
+
+    public static void main(String[] args) {
+        List<Integer> numeros = Arrays.asList(1,2,3,4,5);
+
+        Consumer<Integer> imprimirNumeroPar = numero -> {};
+
+        numeros.forEach(n ->{
+                if(n%2==0){
+                System.out.println(n);
+                }
+            }
+        );
+
+
+
+    }
+}
